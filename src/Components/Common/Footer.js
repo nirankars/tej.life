883a2326b9fe617b434903e6c4764bd146205012
nirankar <nirankar023@gmail.com>
2023-02-01 14:26:47 +0530

    footer spacing

--- src/Components/Common/Footer.js
+++ src/Components/Common/Footer.js
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function Footer () {
     return (
-        <footer className="container mx-auto grid grid-cols-2 text-[#f2f6f9] ">
-            <h1 className="col-start-1">
+        <footer className="container mx-auto grid grid-cols-2 mt-16 mb-8 text-[#f2f6f9] ">
+            <h1 className="col-start-1 text-2xl font-semibold">
             <span>तेज़</span> Life
             </h1>
-            <div className="col-end-4">
+            <div className="col-end-4 text-2xl font-normal">
              <a href="/" className="mr-4">
                     terms
              </a>
